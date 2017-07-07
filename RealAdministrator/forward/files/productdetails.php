9d8c3712924f9fd54cd76b7ddecd119930c34a72
project-web2017002 <optimustechprojectweb201702@gmail.com>
2017-07-07 19:14:57 +0530

    Add files via upload

--- RealAdministrator/forward/files/productdetails.php
+++ RealAdministrator/forward/files/productdetails.php
@@ -1,7 +1,29 @@
-<div class="row">
+<div class="row" id="mainWindow">
     <div class="col-md-12 col-sm-12">
-
-        <div id="txtEditor"></div>
-
+        <form autocomplete="off" method="post">
+            <textarea name="editor" id="editor"></textarea>
+            <input type="text" id="filename" name="filename" placeholder="Enter File name" class="form-control">
+            <select id="fileExt" name="fileExt" class="form-control">
+                <option value=".html">.html</option>
+                <option value=".txt">.txt</option>
+            </select>
+            <button type="submit" name="productAdsubmit" id="productAdsubmit" class="btn btn-default btn-block">Upload</button>
+        </form>
     </div>
 </div>
+
+<?php
+if(isset($_POST['editor'])){
+    $data=$_POST['editor'];
+    $filename = $_POST['filename'];
+    $file_ext = $_POST['fileExt'];
+
+    $path = "newUploads/";
+    mkdir($path, 0, true);
+
+    $createfile = fopen($path."/".$filename.$file_ext,"w");
+    fwrite($createfile, $data);
+    fclose($createfile);
+    header("location://localhost/optimus/RealAdministrator/");
+}
+?>
