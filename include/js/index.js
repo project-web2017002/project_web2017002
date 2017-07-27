ac62ce7b33d132fda82bec1770301059e777b5bc
project-web2017002 <optimustechprojectweb201702@gmail.com>
2017-07-27 20:11:31 +0530

    Add files via upload

--- include/js/index.js
+++ include/js/index.js
@@ -5,25 +5,33 @@ function changename(data){
     var t;
     var ult;
     var parsed;
-    if(urlofpage.indexOf('?') > -1){
-        sep = "&";
+    if(urlofpage.includes('page=')){
         g = data.split('> ');
         t = g[1].split(' <');
-        if(urlofpage.includes('city=')){
-            ult = urlofpage.replace(/(city=)[^\&]+/, '$1' + t[0]);
-            window.location.href = ult;
-        }else{
+        ult = '?city='+t[0];
+        window.location.assign(ult);
+    }
+    else {
+        if (urlofpage.indexOf('?') > -1) {
+            sep = "&";
+            g = data.split('> ');
+            t = g[1].split(' <');
+            if (urlofpage.includes('city=')) {
+                ult = urlofpage.replace(/(city=)[^\&]+/, '$1' + t[0]);
+                window.location.href = ult;
+            } else {
+                parsed = "city=" + encodeURIComponent(t[0]);
+                ult = urlofpage + sep + parsed;
+                window.location.href = ult;
+            }
+        } else {
+            sep = "?";
+            g = data.split('> ');
+            t = g[1].split(' <');
             parsed = "city=" + encodeURIComponent(t[0]);
             ult = urlofpage + sep + parsed;
             window.location.href = ult;
         }
-    } else {
-        sep = "?";
-        g = data.split('> ');
-        t = g[1].split(' <');
-        parsed = "city=" + encodeURIComponent(t[0]);
-        ult = urlofpage + sep + parsed;
-        window.location.href = ult;
     }
     var nevalue = data;
     document.getElementById("cityvalue").innerHTML = nevalue;
@@ -44,19 +52,48 @@ function changecategory(dataa){
     var sep;
     var ult;
     var parsed;
+    if(urlofpage.includes('page=')){
+        ult = '?categoryId='+dataa;
+        window.location.assign(ult);
+    }
+    else {
+        if (urlofpage.indexOf('?') > -1) {
+            sep = "&";
+            if (urlofpage.includes('categoryId=')) {
+                ult = urlofpage.replace(/(categoryId=)[^\&]+/, '$1' + dataa);
+                window.location.href = ult;
+            } else {
+                parsed = "categoryId=" + encodeURIComponent(dataa);
+                ult = urlofpage + sep + parsed;
+                window.location.href = ult;
+            }
+        } else {
+            sep = "?";
+            parsed = "categoryId=" + encodeURIComponent(dataa);
+            ult = urlofpage + sep + parsed;
+            window.location.href = ult;
+        }
+    }
+}
+
+function changepage(dataa){
+    var urlofpage = window.location.href;
+    var sep;
+    var ult;
+    var parsed;
     if(urlofpage.indexOf('?') > -1){
         sep = "&";
-        if(urlofpage.includes('categoryId=')){
-            ult = urlofpage.replace(/(categoryId=)[^\&]+/, '$1' + dataa);
+        if(urlofpage.includes('page=')){
+            ult = urlofpage.replace(/(page=)[^\&]+/, '$1' + dataa);
             window.location.href = ult;
         }else{
-            parsed = "categoryId=" + encodeURIComponent(dataa);
+            parsed = "page=" + encodeURIComponent(dataa);
             ult = urlofpage + sep + parsed;
             window.location.href = ult;
         }
     } else {
         sep = "?";
-        parsed = "categoryId=" + encodeURIComponent(dataa);
+        parsed = "page=" + encodeURIComponent(dataa);
         ult = urlofpage + sep + parsed;
         window.location.href = ult;
     }
