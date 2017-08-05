8169a277daa25fb5ef4e819d1afd6bad950e1e5f
project-web2017002 <optimustechprojectweb201702@gmail.com>
2017-08-05 21:47:21 +0530

    Add files via upload

--- forward/dataindex.php
+++ forward/dataindex.php
@@ -4,29 +4,58 @@ $checkfeatured = mysqli_query($con,"select * from featpro");
 $featuredcount = mysqli_num_rows($checkfeatured);
 ?>
 <div class="container-fluid mainBar-fluid">
-    <div class="row extra"></div>
+
+    <?php
+    if($categoryid == '' && $city == '') {
+        ?>
+        <div class="row extra">
+            <div class="positionadbtn" data-toggle="modal" data-target="#postModal"
+                 style="text-align: center; margin-top:10%">
+                <a id="btnPostAd" href="#" class="btn btn-warning btn-lg" title="Post Free Ad">Post Free Ad</a>
+            </div>
+        </div>
+        <?php
+    }
+    ?>
+
     <div class="row dataindex-main">
-        <div class="col-lg-3 leftsidebar" id="CategoryMenu">
-            <div class="row">
+        <div class="container">
+            <div class="row" id="oo" style="cursor: pointer">
+                <div class="col-md-4 col-sm-12"></div>
+                <div class="col-md-4 col-sm-12" style="text-align: center; padding: 5px 10px">Categories &bigtriangledown;</div>
+                <div class="col-md-4 col-sm-12"></div>
+            </div>
+            <div class="row" id="CategoryMenu">
                 <?php
                 require("include/media/categories.php");
                 ?>
             </div>
-            <div class="row">
-                <div>
-                    <img src="include/media/images/mcd.png" alt="Avertisemant" class="img-responsive">
+        </div>
+    </div>
+
+    <?php
+    if($categoryid == '' && $city == '') {
+        ?>
+        <div class="row" style="padding: 10px">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 col-sm-12 col-xs-12">
+                        <img src="include/media/images/mcd.png" alt="Avertisemant" class="img-responsive">
+                    </div>
+                    <div class="col-md-9 col-sm-12 col-xs-12">
+                        <?php include("carousal.php"); ?>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-lg-9 rightsidebar" id="mainWindow">
+        <?php
+    }
+    ?>
+
+    <div class="row">
+        <div class="rightsidebar" id="mainWindow">
             <?php
             if($categoryid=='' && $city==''){
-            include("carousal.php");
-            ?>
-            <div class="padmar" style="text-align: -webkit-center">
-                <img src="include/media/images/apple.jpg" alt="Avertisemant" class="img-responsive">
-            </div>
-                <?php
                 if($featuredcount > 0) {
                     ?>
                     <div class="text-primary" style="text-align: center; text-transform: capitalize"><h3>Featured Products</h3></div>
@@ -45,11 +74,6 @@ $featuredcount = mysqli_num_rows($checkfeatured);
                 require("Filter/index.php");
             }
             ?>
-        </div>
-    </div>
-    <div class="row">
-        <div class="padmar" style="text-align: -webkit-center">
-            <img src="include/media/images/ai-coverpic.jpg" alt="Avertisemant" class="img-responsive">
         </div>
     </div>
 </div>
