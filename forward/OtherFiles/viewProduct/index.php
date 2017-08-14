76def190e0801bf18be31d81f98f8df7b2df733f
project-web2017002 <optimustechprojectweb201702@gmail.com>
2017-08-14 19:55:04 +0530

    Add files via upload

--- forward/OtherFiles/viewProduct/index.php
+++ forward/OtherFiles/viewProduct/index.php
@@ -1,17 +1,18 @@
 <script src="//localhost/optimus/include/js/product.js"></script>
-<div class="container" id="viewProduct" style="width:100%; height: 100%; display: none; position: fixed; z-index:9999; top:0; left:0; margin: 0; padding: 10px; ">
-    <div class="viewProductHeader" style="text-align: right; font-size: larger; cursor: pointer; padding: 10px">&times; Close</div>
+
+<div class="modal" id="viewProduct">
+    <div class="viewProductHeader">&times; Close</div>
     <div class="errorViewProduct"></div>
     <div class="viewProductBody">
         <div class="container">
             <div class="row"><div class="col-sm-12"><div id="vieproducterror"></div></div></div>
             <div class="row">
-                <div class="col-md-4 col-sm-12" style="text-align: -webkit-center;" id="img-tab"></div>
+                <div class="col-md-4 col-sm-12" id="img-tab"></div>
                 <div class="col-md-8 col-sm-12">
                     <div class="container-fluid">
                         <div class="hidden" id="_jkpr"></div>
                         <div class="row">
-                            <div class="col-sm-12" style="font-size: larger; font-weight: 700" id="viewproductTitle"></div>
+                            <div class="col-sm-12" id="viewproductTitle"></div>
                         </div>
                         <?php
                         if($id != '') {
--- forward/modal.php
+++ forward/modal.php
@@ -6,6 +6,38 @@ include("signup/signup.php");
 include("postFreeAD/adBlockModal.php");
 include("login/logoutmodal.php");
 include("profile/edit.php");
+
+
+function time_elapsed_string($datetime, $full = false) {
+    $now = new DateTime;
+    $ago = new DateTime($datetime);
+    $diff = $now->diff($ago);
+
+    $diff->w = floor($diff->d / 7);
+    $diff->d -= $diff->w * 7;
+
+    $string = array(
+        'y' => 'year',
+        'm' => 'month',
+        'w' => 'week',
+        'd' => 'day',
+        'h' => 'hour',
+        'i' => 'minute',
+        's' => 'second',
+    );
+    foreach ($string as $k => &$v) {
+        if ($diff->$k) {
+            $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+        } else {
+            unset($string[$k]);
+        }
+    }
+
+    if (!$full) $string = array_slice($string, 0, 1);
+    return $string ? implode(', ', $string) . ' ago' : 'just now';
+}
+
+
 ?>
 <div id="myModal" class="modal fade" role="dialog">
     <div class="modal-dialog" id="moda">
@@ -18,11 +50,11 @@ include("profile/edit.php");
             <div class="modal-body smalltext">
                 <h3>Already a Customer?</h3>
                 <div class="container">
-                    <button class="btn btn-danger btn-lg btn-block" data-toggle="modal" data-target="#loginModal" data-dismiss="modal">Login</button>
+                    <button class="btn btn-danger btn-lg btn-block" onclick="one()">Login</button>
                 </div>
                 <h3>New User?</h3>
                 <div class="container">
-                    <button class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#signupModal" data-dismiss="modal">Signup</button>
+                    <button class="btn btn-success btn-lg btn-block" onclick="two()">Signup</button>
                 </div>
             </div>
         </div>
